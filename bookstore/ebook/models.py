758d1f3e3e83b321fdb605e16ce204f9ea3b6deb
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-21 14:18:43 -0800

    update

--- bookstore/ebook/models.py
+++ bookstore/ebook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    posted_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return str(self.message)
+
