7fdbe7afdb96309549ece49592fb47bc957807d8
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-22 12:24:18 -0800

    modeling

--- bookstore/ebook/admin.py
+++ bookstore/ebook/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Chat
+from .models import Chat, Tbook, Rbook, Deleterequest, Feedback
+
 
 # Register your models here.
-admin.site.register(Chat)
+admin.site.register(Chat),
+admin.site.register(Tbook),
+admin.site.register(Rbook)
+admin.site.register(Deleterequest)
+admin.site.register(Feedback)
--- bookstore/ebook/models.py
+++ bookstore/ebook/models.py
@@ -1,6 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth import get_user_model
+
 # Create your models here.
+
+# class User(AbstractUser):
+#     is_admin = models.BooleanField(default=False)
+#     is_publisher = models.BooleanField(default=False)
+#     is_librarian = models.BooleanField(default=False)
+#     is_student  = models.BooleanField(default=False)
+
+#     class Meta:
+#         swappable = 'AUTH_USER_MODEL'
+
+class Tbook(models.Model):
+    grade = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    uploaded_by = models.CharField(max_length=100, null=True, blank=True)
+    pdf = models.FileField(upload_to='ebook/media/tbook/pdfs')
+    cover = models.ImageField(upload_to='ebook/media/tbook/covers', null=True, blank=True)
+
+    def __str__(self):
+        return self.grade
+    
+    def delete(self, *args, **kwargs):
+        self.pdf.delete()
+        self.cover.delete()
+        super().delete(*args, **kwargs)
+
+class Rbook(models.Model):
+    title = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    uploaded_by = models.CharField(max_length=100, null=True, blank=True)
+    pdf = models.FileField(upload_to='ebook/media/rbook/pdfs')
+    cover = models.ImageField(upload_to='ebook/media/rbook/covers', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def delete(self, *args, **kwargs):
+        self.pdf.delete()
+        self.cover.delete()
+        super().delete(*args, **kwargs)
 
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +55,14 @@ class Chat(models.Model):
     def __str__(self):
         return str(self.message)
 
+class Deleterequest(models.Model):
+    delete_request = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.delete_request
+
+class Feedback(models.Model):
+    feedback = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.feedback
