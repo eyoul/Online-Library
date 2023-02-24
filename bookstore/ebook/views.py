160a193227394d579622c295a64693641dc3105b
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-24 13:42:06 -0800

    user

---
+++ bookstore/ebook/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+# login and logout .
+def logio(request):
+    return render(request, 'auth/logio.html', {})
