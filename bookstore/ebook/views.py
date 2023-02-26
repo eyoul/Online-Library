903ef872894887b2096525bce87217fb97149b70
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-26 03:15:02 -0800

    base html

--- bookstore/ebook/views.py
+++ bookstore/ebook/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from . import views
 
 # Create your views here.
 def index(request):
-    return render(request, 'dashboard/base.html', {})
+    return render(request, 'librarian/base.html', {})
 
 # login and logout .
 def logio(request):
@@ -13,4 +14,11 @@ def about(request):
     return render(request, 'about.html', {})
 
 def gchat(request):
-    return render(request, 'group_chat.html', {})
+    return render(request, 'librarian/group_chat.html', {})
+
+def tbook(request):
+    return render(request, 'librarian/add_tbook.html', {})
+
+def rbook(request):
+    return render(request, 'librarian/add_rbook.html', {})
+
