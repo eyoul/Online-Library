519dbd6f59fe1105f90e007b420c60decb72eab0
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-27 15:42:50 -0800

    Update forms.py

--- bookstore/ebook/forms.py
+++ bookstore/ebook/forms.py
@@ -1,3 +1,7 @@
-
-
-
+from django import forms
+from django.forms import ModelForm
+from .models import Rbook
+class RbookForm(ModelForm):
+    class Meta:
+        model = Rbook
+        fields = "__all__"
