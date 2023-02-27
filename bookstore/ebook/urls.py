a4da8f456d7b9f377aad584f2758b240b4c597e7
abelasfaw <abeloman89@gmail.com>
2023-02-28 00:30:52 +0300

    added rbook paths

--- bookstore/ebook/urls.py
+++ bookstore/ebook/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 urlpatterns = [
-   path('', views.index, name='index'),
-   path('logio', views.logio, name='logio'),
-   path('about', views.about, name='about'),
-  
+    path("", views.index, name="index"),
+    path("logio", views.logio, name="logio"),
+    path("about", views.about, name="about"),
+    path("rbook", views.addRbook, name="addRbook"),
+    path("list_books", views.listBooks, name="list_books"),
+    path("book", views.Book.as_view(), name="book"),
 ]
